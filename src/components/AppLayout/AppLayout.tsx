9c2c84274b0d287cd36582cefce13cbcc761bc00
Chandrika C <chandrika@Chandrikas-MacBook-Air.local>
2025-11-10 14:35:36 +0530

    Add AppLayout component for consistent layout structure; refactor HomePage and TrendingPage to utilize AppLayout for improved organization and maintainability.

---
+++ src/components/AppLayout/AppLayout.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from 'react'
+import Navbar from '../Navbar/Navbar'
+import Sidebar from '../Sidebar/Sidebar'
+
+interface AppLayoutProps {
+  children: ReactNode
+  headerContent?: ReactNode
+  headerClassName?: string
+}
+
+const AppLayout = ({ children, headerContent, headerClassName = '' }: AppLayoutProps) => {
+  return (
+    <div className="h-screen bg-white dark:bg-black text-black dark:text-white transition-colors duration-300 flex flex-col overflow-hidden">
+      <Navbar />
+      <div className="flex flex-1 overflow-hidden">
+        <Sidebar />
+        <main className="flex-1 flex flex-col overflow-hidden">
+          {headerContent && (
+            <div className={`sticky top-0 z-40 bg-white dark:bg-black px-4 lg:px-8 pt-4 pb-4 border-b border-gray-200 dark:border-gray-800 ${headerClassName}`}>
+              {headerContent}
+            </div>
+          )}
+          <div className="flex-1 overflow-y-auto px-4 lg:px-8 pb-4">
+            {children}
+          </div>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default AppLayout
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef } from 'react'
 import { observer } from 'mobx-react-lite'
-import Navbar from '../components/Navbar/Navbar'
-import Sidebar from '../components/Sidebar/Sidebar'
+import AppLayout from '../components/AppLayout/AppLayout'
 import SearchBar from '../components/SearchBar/SearchBar'
 import VideoList from '../components/VideoList/VideoList'
 import videoModel from '../stores/VideoModel'
@@ -18,20 +17,9 @@ const HomePage = observer(() => {
   }, [])
 
   return (
-    <div className="h-screen bg-white dark:bg-black text-black dark:text-white transition-colors duration-300 flex flex-col overflow-hidden">
-      <Navbar />
-      <div className="flex flex-1 overflow-hidden">
-        <Sidebar />
-        <main className="flex-1 flex flex-col overflow-hidden">
-          <div className="sticky top-0 z-40 bg-white dark:bg-black px-4 lg:px-8 pt-4 pb-4 border-b border-gray-200 dark:border-gray-800">
-            <SearchBar />
-          </div>
-          <div className="flex-1 overflow-y-auto px-4 lg:px-8 pb-4">
-            <VideoList model={videoModel} />
-          </div>
-        </main>
-      </div>
-    </div>
+    <AppLayout headerContent={<SearchBar />}>
+      <VideoList model={videoModel} />
+    </AppLayout>
   )
 })
 
